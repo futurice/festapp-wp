e712bbc68875319a92ca7622e9ad846017f4cf03
Esa Tanskanen <esa.tanskanen@futurice.com>
2014-04-11 21:57:20 +0300

    DataLoader implementation started

---
+++ FestApp/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FestApp
+{
+    public class Config
+    {
+        public static string ServerUrl = "http://festapp-server.herokuapp.com/api/";
+    }
+}
---
+++ FestApp/DataLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace FestApp
+{
+    public class DataLoader
+    {
+        public async Task<T> Load<T>(string apiPath)
+        {
+            WebClient web = new WebClient();
+            //web.OpenReadCompleted += new OpenReadCompletedEventHandler(RequestComplete);
+            web.OpenReadAsync(new Uri(Config.ServerUrl + apiPath));
+        }
+    }
+}
