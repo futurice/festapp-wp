7e8914fbcc32d1c391bf3e6b1f5fa5aa34362312
Sakari Bergen <sakari.bergen@futurice.com>
2014-04-12 21:36:56 +0300

    Add AsyncLock, AsyncSemaphore and LazyAsync (LazyAsync not tested yet)

---
+++ FestApp/Utils/AsyncLock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FestApp.Utils
+{
+    // "Scoped" async locking, inspired by
+    // http://blogs.msdn.com/b/pfxteam/archive/2012/02/12/10266988.aspx
+    public class AsyncLock
+    {
+        private readonly AsyncSemaphore _semaphore;
+        private readonly Releaser _releaser;
+
+        public struct Releaser : IDisposable
+        {
+            private readonly AsyncLock _toRelease;
+
+            internal Releaser(AsyncLock toRelease) { _toRelease = toRelease; }
+
+            public void Dispose()
+            {
+                if (_toRelease != null)
+                    _toRelease._semaphore.Release();
+            }
+        }
+
+        public AsyncLock()
+        {
+            _semaphore = new AsyncSemaphore(1);
+            _releaser = new Releaser(this);
+        }
+
+        public Releaser? TryLock()
+        {
+            if (_semaphore.TryWait())
+            {
+                return _releaser;
+            }
+            return null;
+        }
+
+        public async Task<Releaser> LockAsync()
+        {
+            await _semaphore.WaitAsync();
+            return _releaser;
+        }
+    }
+}
---
+++ FestApp/Utils/LazyAsync.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestApp.Utils
+{
+    class LazyAsync<TValue> where TValue : class
+    {
+        private TValue _val = null;
+        private readonly AsyncLock _lock = new AsyncLock();
+        private readonly Func<Task<TValue>> _factory;
+
+        public LazyAsync(Func<Task<TValue>> factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task<TValue> Get()
+        {
+            using (await _lock.LockAsync())
+            {
+                if (_val != null) { return _val; }
+                _val = await _factory();
+            }
+
+            return _val;
+        }
+    }
+}
