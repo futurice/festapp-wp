5547ab694580bff059088e8f3ea535473e45459f
Sakari Bergen <sakari.bergen@futurice.com>
2014-04-12 12:24:42 +0300

    Update models to work against new API

--- FestApp/Models/Artist.cs
+++ FestApp/Models/Artist.cs
@@ -2,32 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace FestApp.Models
 {
     public class Artist
     {
+        [JsonProperty("_id")]
         public string Id { get; set; }
+
         public string Name { get; set; }
+
         public string Picture { get; set; }
+
         public string Quote { get; set; }
+
         public string Content { get; set; }
+
         public string Featured { get; set; }
+
         public string Status { get; set; }
+
         public string Stage { get; set; }
+
         public string Day { get; set; }
-        public string TimeStart { get; set; }
-        public string TimeStop { get; set; }
+
+        [JsonProperty("time_start")]
+        public DateTimeOffset TimeStart { get; set; }
+
+        [JsonProperty("time_stop")]
+        public DateTimeOffset TimeStop { get; set; }
+
         public string Founded { get; set; }
+
         public string Genre { get; set; }
-        public string Members { get; set; }
-        public string Albums { get; set; }
+
+        public List<string> Members { get; set; }
+
+        public List<string> Albums { get; set; }
+
         public string Highlights { get; set; }
+
         public string Youtube { get; set; }
+
         public string Spotify { get; set; }
+
         public string ContactInfo { get; set; }
+
         public string PressImage { get; set; }
+
         public string Credits { get; set; }
+
         public string Place { get; set; }
     }
 }
--- FestApp/Models/NewsItem.cs
+++ FestApp/Models/NewsItem.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace FestApp.Models
 {
     public class NewsItem
     {
+        [JsonProperty("_id")]
         public string Id { get; set; }
+
         public string Title { get; set; }
+
         public string Image { get; set; }
+
+        [JsonProperty("teaser_text")]
         public string TeaserText { get; set; }
+
         public string Content { get; set; }
-        public string Time { get; set; }
+
+        public DateTimeOffset Time { get; set; }
+
         public string Status { get; set; }
     }
 }
