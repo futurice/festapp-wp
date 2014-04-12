cf9399da4e4778b0273d7c922eb502e1bebfaa9b
Sakari Bergen <sakari.bergen@futurice.com>
2014-04-12 12:47:12 +0300

    Add program Model

---
+++ FestApp/Models/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace FestApp.Models
+{
+    class Program
+    {
+        public string Title { get; set; }
+
+        [JsonProperty("cover_image")]
+        public string CoverImage { get; set; }
+
+        public string Content { get; set; }
+
+        public string Place { get; set; }
+
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+    }
+}
